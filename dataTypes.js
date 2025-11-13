b70058cbc6baed710110ddde1845bf0ebced7bb9
Muhammad Umar <muprogrammer295@gmail.com>
2025-11-13 17:06:16 +0500

    variables and constant

---
+++ dataTypes.js
@@ -0,0 +1,9 @@
+const accountId = 112345;
+let accountName = "Umar";
+var accountEmail = "umar@google.com";
+accountCity = "Lahore";
+
+// accountId=2; because we use contant keyword
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountName, accountCity]);
